f12641bc519c4972c4cde35236f99493ab472724
7mmsingh31246 <mmsingh31246@gmail.com>
2016-12-15 08:56:49 +0530

    Delete factory.js

--- app/factory.js
+++
@@ -1,9 +0,0 @@
-angular.module("myApp")
-.factory("asciireader", function(){
-	return {
-		charCodeAt : function(str,i){
-			var c = str.charCodeAt(i);
-			return (c>99?c:"0"+c).toString();
-		}
-	}
-});
